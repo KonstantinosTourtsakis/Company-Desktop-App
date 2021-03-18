0609e015a46747bf9680ccd9bed0b7c65faf6bef
Konstantinos Tourtsakis <72460501+KonstantinosTourtsakis@users.noreply.github.com>
2021-03-18 17:12:34 +0200

    Delete Monitoring App directory

--- Monitoring App/server.py
+++
@@ -1,9 +0,0 @@
-from vidstream import StreamingServer
-
-receiver = StreamingServer('192.168.1.69', 9999)
-receiver.start_server()
-
-print("Server is running... ")
-while input("") != "exit()":
-    continue
-receiver.stop_server()
